1de99ee0654aaff235fba6f5ee669223bfd94e99
hernan066 <hernan0066@gmail.com>
2023-09-29 14:18:09 -0300

    Add config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,57 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/* eslint-disable no-undef */
+import { defineConfig, transformWithEsbuild } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    // Workaround
+    {
+      name: "load+transform-js-files-as-jsx",
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) {
+          return null;
+        }
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: "jsx",
+          jsx: "automatic", // 👈 this is important
+        });
+      },
+    },
+    // End workaround
+  ],
+
+  // Workaround before renaming .js to .jsx
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
+  // End workaround
+  resolve: {
+    alias: {
+      components: `${path.resolve(__dirname, "./src/components/")}`,
+      public: `${path.resolve(__dirname, "./public/")}`,
+      pages: path.resolve(__dirname, "./src/pages"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      examples: path.resolve(__dirname, "./src/examples"),
+      layouts: path.resolve(__dirname, "./src/layouts"),
+      context: path.resolve(__dirname, "./src/context"),
+      utils: path.resolve(__dirname, "./src/utils"),
+      api: path.resolve(__dirname, "./src/api"),
+      router: path.resolve(__dirname, "./src/router"),
+      reduxToolkit: path.resolve(__dirname, "./src/reduxToolkit"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      data: path.resolve(__dirname, "./src/data"),
+      validations: path.resolve(__dirname, "./src/validations"),
+    },
+  },
+});
